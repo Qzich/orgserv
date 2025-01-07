0390568d5679aa4b303e9fc93f7d69c8c7011ae7
Yurii Kuzich <qzichs@gmail.com>
2025-01-07 15:26:47 +0100

    users done

--- apps/users/main.go
+++ apps/users/main.go
@@ -7,17 +7,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/qzich/orgserv/apps/users/internal/api/controller"
 	"github.com/qzich/orgserv/apps/users/internal/api/router"
 	"github.com/qzich/orgserv/apps/users/internal/pkg/repository/mysql"
 	"github.com/qzich/orgserv/apps/users/internal/pkg/service"
-	"github.com/qzich/orgserv/entity/users"
 	"github.com/qzich/orgserv/pkg/api/json"
 	logger "github.com/qzich/orgserv/pkg/logger/impl"
-	pkgservice "github.com/qzich/orgserv/pkg/service"
-	"github.com/qzich/orgserv/pkg/uuid"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -46,7 +42,7 @@ func main() {
 	log.Info(ctx, "Run users service")
 
 	//createUser(ctx, userService)
-	fmt.Println(getUsers(db))
+	// fmt.Println(getUsers(ctx, userService))
 
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		if errors.Is(err, http.ErrServerClosed) {
@@ -58,50 +54,18 @@ func main() {
 	}
 }
 
-func getUsers(db *sql.DB) []users.User {
-	type userDAO struct {
-		ID        int64
-		UserId    string
-		Name      string // required, min 4, max 255
-		Email     string // required, email format
-		Kind      string // required, support, customer
-		CreatedAt time.Time
-		UpdatedAt time.Time
-	}
+// func getUsers(ctx context.Context, pkgservice pkgservice.UsersService) []users.User {
+// 	users, err := pkgservice.AllUsers(ctx)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	var res []users.User
-	rows, err := db.Query("SELECT id, user_id, name, email, kind, created_at, updated_at FROM users")
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
+// 	return users
+// }
 
-	for rows.Next() {
-		var user userDAO
-		if err := rows.Scan(&user.ID, &user.UserId, &user.Name, &user.Email, &user.Kind, &user.CreatedAt, &user.UpdatedAt); err != nil {
-			panic(err)
-		}
-
-		userID, err := uuid.FromString(user.UserId)
-		if err != nil {
-			panic(err)
-		}
-
-		res = append(res, users.User{
-			ID:        userID,
-			Name:      user.Name,
-			Email:     user.Email,
-			Kind:      user.Kind,
-			CreatedAt: user.CreatedAt,
-			UpdatedAt: user.UpdatedAt,
-		})
-	}
-	return res
-}
-
-func createUser(ctx context.Context, pkgservice pkgservice.UsersService) {
-	_, err := pkgservice.CreateUser(ctx, "first", "qzichs@gmail.com", "support")
-	if err != nil {
-		panic(err)
-	}
-}
+// func createUser(ctx context.Context, pkgservice pkgservice.UsersService) {
+// 	_, err := pkgservice.CreateUser(ctx, "first", "qzichs@gmail.com", "support")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
